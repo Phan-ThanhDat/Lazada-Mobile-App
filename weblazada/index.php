ded2b6891534577eb8ff9c8ad6115022ba5fb283
Duc Phan <phduc.phanhongduc@gmail.com>
2017-10-27 16:27:34 +0300

    Demo create JSON by code PHP

---
+++ weblazada/index.php
@@ -0,0 +1,5 @@
+<?php 
+	$sanpham =  array(array('emplid' => "SJ011MS", "personal"=>array('name'=>"SmithSon")), array('emplid' => "SJ012MS", "personal"=>array('name'=>"Allan") ));
+	echo json_encode($sanpham);
+	
+ ?>
